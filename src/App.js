66e51f3ab2a2b640b8ee5d966e1556c20a1b1456
Marvin Yan <marvinsyan@gmail.com>
2022-04-21 00:33:19 -0400

    Reduce dataset using BFS and display 3D network graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { ForceGraph3D } from 'react-force-graph';
+import { csv } from 'd3';
 import './App.css';
+import ellipticNodes from './datasets/elliptic_txs_classes.csv';
+import ellipticEdges from './datasets/elliptic_txs_edgelist.csv';
+import { formatData } from './utils';
 
-function App() {
+const App = () => {
+  // call formatData and save the result in a variable
+  // use the useEffect hook to call the formatData function
+  const [rawData, setRawData] = useState();
+  const [seedTxId, setSeedTxId] = useState("355110272")
+
+  useEffect(() => {
+    csv(ellipticEdges).then((data) => {
+      setRawData(data);
+    });
+  }, []);
+
+  const graphData = rawData ? formatData(rawData, seedTxId) : null
+  console.log(graphData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      {graphData ? <ForceGraph3D graphData={graphData} /> : null}
     </div>
   );
-}
+};
 
 export default App;
+
+// d3.csv(ellipticNodes).then((data) => {
+//   for (let i = 0; i < 5; i++) {
+//     const node = data[i];
+//     usedTxIds.add(node.txId);
+
+//     let label = node.class;
+//     if (label === 'unknown') {
+//       label = 'Unknown';
+//     } else if (label === '1') {
+//       label = 'Illicit';
+//     } else {
+//       label = 'Licit';
+//     }
+
+//     output.nodes.push({
+//       id: node.txId,
+//       name: `${node.txId} - ${label}`,
+//     });
+//   }
+// });
+
+// d3.csv(ellipticEdges).then((data) => {
+//   data.forEach((edge) => {
+//     if (usedTxIds.has(edge.txId1) || usedTxIds.has(edge.txId2)) {
+//       output.links.push({
+//         source: edge.txId1,
+//         target: edge.txId2,
+//       });
+//     }
+//   });
+// });
---
+++ src/utils/index.js
@@ -0,0 +1,71 @@
+/* 
+Returns an object that looks like 
+{
+  "nodes": [ 
+      { 
+        "id": "id1",
+        "name": "name1",
+      },
+      { 
+        "id": "id2",
+        "name": "name2",
+      },
+      ...
+  ],
+  "links": [
+      {
+          "source": "id1",
+          "target": "id2"
+      },
+      ...
+  ]
+}
+
+The nodes come from ./datasets/elliptic_txs_classes.csv which has columns txId, class
+The edges come from ./datasets/elliptic_txs_edgelist.csv which has columns txId1, txId2
+*/
+export const formatData = (data, seedTxId, maxNodes=1000) => {
+  const output = { nodes: [], links: [] };
+
+  const adjacencyList = {};
+  data.forEach((edge) => {
+    const { txId1, txId2 } = edge;
+
+    if (!adjacencyList[txId1]) {
+      adjacencyList[txId1] = [txId2];
+    } else {
+      adjacencyList[txId1].push(txId2);
+    }
+
+    if (!adjacencyList[txId2]) {
+      adjacencyList[txId2] = [txId1];
+    } else {
+      adjacencyList[txId2].push(txId1);
+    }
+  });
+
+  // Do breadth-first search starting from seedTxId.
+  const queue = [seedTxId];
+  const visited = new Set();
+  visited.add(seedTxId);
+
+  while (visited.length < maxNodes) {
+    const current = queue.shift();
+    if (current) {
+      const neighbors = adjacencyList[current];
+
+      if (neighbors) {
+        neighbors.forEach((neighbor) => {
+          if (!visited.has(neighbor)) {
+            visited.add(neighbor);
+            queue.push(neighbor);
+            output.links.push({ source: current, target: neighbor });
+          }
+        });
+      }
+    }
+  }
+
+  output.nodes = Array.from(visited).map((id) => ({ id }));
+  return output;
+};
